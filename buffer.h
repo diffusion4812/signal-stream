0311e6daffdf9a03dbaa8eaafac2dab855e6e2bf
Arthur Loukes <arthurloukes@gmail.com>
2025-09-08 22:19:11 -0500

    Add circular buffer classes and logging functionality
    Introduce `SPSC_CircularBuffer` and `BlockingCircularBuffer` in `buffer.cpp` for efficient data handling. Add a `Console` class in `buffer.h` for prioritized logging. Update `CMakeLists.txt` to include the Boost library directory for new dependencies.

---
+++ buffer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Console {
+    typedef struct {
+        int prio;
+        std::string text;
+    } LogItem;
+
+public:
+    void log(int prio, std::string text);
+    int getCount() const;
+    LogItem getItem(int index) const;
+    void removeItem(int index);
+    void removeAll();
+    void getItemsAsString(std::string& out) const;
+
+private:
+    std::vector<LogItem> logItems;
+};
